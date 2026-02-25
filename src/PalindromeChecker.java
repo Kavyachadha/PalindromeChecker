765ae2c476c7cf46b5e6d7a2d82cb69918a22d42
kavyachadha <kavyachadhaa@gmail.com>
2026-02-25 10:13:51 +0530

    UC3:Palindrome checker using string reversal

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -2,17 +2,18 @@ public class PalindromeChecker {
     public static void main(String[] agrs){
 
         String input = "nitin";
-        boolean isPalindrome = true;
+        String reversed = "";
 
-        // Loop only till half of the string length
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        // Reverse the string
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
-
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        if (input.equals(reversed)) {
+            System.out.println("Input text: " + input);
+            System.out.println("It is a Palindrome");
+        } else {
+            System.out.println("Input text: " + input);
+            System.out.println("It is NOT a Palindrome");
+        }
     }
 }
