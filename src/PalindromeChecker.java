8737f469932b2332766d2e0eed0047c3751dffcf
kavyachadha <kavyachadhaa@gmail.com>
2026-02-25 10:02:44 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,6 +1,18 @@
 public class PalindromeChecker {
     public static void main(String[] agrs){
-        System.out.println("Welcome to Palindrome Checker");
-        System.out.println("Version 1.00");
+
+        String input = "nitin";
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
